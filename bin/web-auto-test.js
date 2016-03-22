ce73f6623d9aefe1f3975ca9de1af8a0580cf937
yunfei <adventure030@gmail.com>
2016-03-22 13:45:22 +0800

    update cli "test" command

--- bin/web-auto-test.js
+++ bin/web-auto-test.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 
-var child_process = require('child_process');
-child_process.spawn('gulp', process.argv.slice(2), {stdio: "inherit"});
+/*
+ * 使用方式参照 gulpfile (除了 "test" 命令)
+ */
+
+var child_process = require('child_process'),
+    path = require('path');
+
+var gulpfilePath = path.join(__dirname, '..', 'gulpfile.js');
+
+if (process.argv[2] === 'test') {
+    // gulp task and webdriverio test conficts with "cwd" setting, we should manually handle this case
+    var wdioConfigFile = process.argv[3];
+    if (wdioConfigFile) {
+        var wdio_cli_path = path.join(__dirname, '..', 'node_modules', '.bin', 'wdio')
+        child_process.spawn('node', [wdio_cli_path, wdioConfigFile], {stdio: "inherit"});
+    } else {
+        console.log('Usage: web-auto-test test <wdioConfigFile>');
+    }
+} else {
+    child_process.spawn('gulp', ['--gulpfile', gulpfilePath].concat(process.argv.slice(2)), {stdio: "inherit"});
+}
